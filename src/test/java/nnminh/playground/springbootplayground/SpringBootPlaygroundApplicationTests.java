ba3a7fa70ba04e9641b7481fb8b281cc7d2f3f0c
nnminh-haise <nnminh.sam.1803@gmail.com>
2024-07-20 10:34:28 +0700

    feature: initialize project with spring web and lombok dependency.

---
+++ src/test/java/nnminh/playground/springbootplayground/SpringBootPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package nnminh.playground.springbootplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
